d66fe555958b58a7ab054468ceb5435367250083
Alexander <alex.galev@yahoo.com>
2023-02-21 11:31:03 -0700

    pushing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import React from "react";
-import Article from "./Article";
-import Comment from "./Comment";
+import React from 'react'
+import Article from './Article'
+import Comment from './Comment'
 
-function App() {
-  return (
-    <div>
-      <Article />
-      <Comment />
-    </div>
-  );
+export default function App() {
+	return (
+		<div>
+			<Article />
+			<Comment />
+		</div>
+	)
 }
-
-export default App;
